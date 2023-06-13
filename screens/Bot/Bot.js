d8f6dddd716d6ff8bbc40d2a08cb92a9fe9d5791
Irfan Khan <jarvismsk@gmail.com>
2023-06-13 14:14:55 +0530

    Started working on Doctor

--- screens/Bot/Bot.js
+++ screens/Bot/Bot.js
@@ -1,17 +1,32 @@
-import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, Linking } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput } from 'react-native';
+import axios from 'axios';
+import { SafeAreaView } from 'react-native';
 
-export default function mindbot(){
-    return(
-        <View style={styles.bot}>
-            <Text>Bot Page</Text>
-        </View>
-    )
+const demo = () => {
+    const [text, onChangeText] = React.useState('');
+
+
+return (
+    <SafeAreaView>
+        <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        placeholder='Type-your message'
+        value={text}/>
+    <button>Submit</button>
+    </SafeAreaView>
+)
 }
 
 const styles = StyleSheet.create({
-    bot: {
-        backgroundColor: 'blue',
-    }
+    input: {
+      height: 40,
+      marginTop: 650,
+      borderWidth: 1,
+      padding: 10,
+      alignContent: "space-around"
+    },
+  });
 
-})
+export default demo;
